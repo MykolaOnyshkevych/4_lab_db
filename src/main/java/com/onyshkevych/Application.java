3710118df5edc7787ab5aceaa5f264ab36c2208a
MykolaOnyshkevych <57072856+MykolaOnyshkevych@users.noreply.github.com>
2021-05-18 15:42:20 +0300

    Update Application.java

--- src/main/java/com/onyshkevych/Application.java
+++ src/main/java/com/onyshkevych/Application.java
@@ -2,12 +2,16 @@ package com.onyshkevych;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class Application {
-
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Running Spring Boot Application");
+    }
 }
